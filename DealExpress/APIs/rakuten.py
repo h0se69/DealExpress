61b12ce422ce6e2b34b98ac62c0d09678600eacd
h0se_69 <joseplaysmc99@gmail.com>
2022-11-22 20:11:42 -0800

    API + JS code reduction
    > Looked over API code and reduced the clutter + APICalls.js less
    clutter of code

--- DealExpress/APIs/rakuten.py
+++ DealExpress/APIs/rakuten.py
@@ -15,21 +15,8 @@ class Rakuten():
     Get Rakuten Cashback based on retailer (Priority Method)
     """
     def rakutenCashBack(self):
-        headers = {
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Referer': 'https://www.rakuten.com/',
-            'Sec-Fetch-Dest': 'script',
-            'Sec-Fetch-Mode': 'no-cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-            'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-        }
+        headers = { 'Accept': '*/*', 'Accept-Language': 'en-US,en;q=0.9', 'Connection': 'keep-alive', 'Referer': 'https://www.rakuten.com/', 'Sec-Fetch-Dest': 'script', 'Sec-Fetch-Mode': 'no-cors', 'Sec-Fetch-Site': 'same-site', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36', 'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"', }
         jsonpValue = f"jsonp_{self.genUUID()}"
-
         # Request error = falls back to brute froce method
         try:
             rakutenResponse = self.session.get(f'https://rewards.api.search-suggest.rakuten.com/search/v2/us_rewards_search/store.json?query=cf_text:%22{self.retailer}%22&sid=us_rewards_ac_001&json.wrf={jsonpValue}', headers=headers)
@@ -53,7 +40,7 @@ class Rakuten():
                 pass
         else:
             return {
-                "ERROR": f"{self.retailer.upper}_NOT_AVAILABLE_FOR_CASHBACK_REWARDS"
+                "ERROR": f"{self.retailer.upper()}_NOT_AVAILABLE_FOR_CASHBACK_REWARDS"
             }
 
     """
@@ -61,24 +48,10 @@ class Rakuten():
     if rakuten_StoreSearch() were to timeout/fail
     """
     def rakutenCashBack_BruteForce(self):
-        headers = {
-            'authority': 'www.rakuten.com',
-            'accept': 'application/json, text/plain, */*',
-            'accept-language': 'en-US,en;q=0.9',
-            'client-agent': 'rr-bih-feweb',
-            'referer': 'https://www.rakuten.com/stores/all',
-            'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-            'x-anonymous-id': self.genUUID(),
-            'x-platform': 'DESKTOP_WEB',
-        }
+        headers = { 'authority': 'www.rakuten.com', 'accept': 'application/json, text/plain, */*', 'accept-language': 'en-US,en;q=0.9', 'client-agent': 'rr-bih-feweb', 'referer': 'https://www.rakuten.com/stores/all', 'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'same-origin', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36', 'x-anonymous-id': self.genUUID(), 'x-platform': 'DESKTOP_WEB', }
         cursorString = ""
         start = 0
+        
         params = {
             'topicId': '16867',
             'entityIds': '',
